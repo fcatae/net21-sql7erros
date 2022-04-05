9331f0887fb20ed2099dcd2b481989a9f2391202
Fabricio Catae <fcatae@hotmail.com>
2022-04-05 17:52:19 -0300

    initial commit

---
+++ database.sql
@@ -0,0 +1,23 @@
+create table tbLogin
+(
+  id int primary key identity,
+  username varchar(100),
+  email varchar(100),
+  pwd varchar(100)
+);
+
+insert tbLogin(username, email, pwd)
+values ('admin', 'teste@domain.com.br', 'fiap');
+GO
+
+create table tbLog(description varchar(1000));
+GO
+
+create user app with password='<password>'
+create user aclogin with password='<password>'
+grant select on tbLogin to aclogin
+grant select, insert on tbLog to app
+
+
+
+
