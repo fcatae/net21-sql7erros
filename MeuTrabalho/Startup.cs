dcb9124d67f5811ef4363d782aa91f3eba1e8bec
Fabricio Catae <fcatae@hotmail.com>
2022-04-05 20:10:35 -0300

    upgrade NET5

--- MeuTrabalho/Startup.cs
+++ MeuTrabalho/Startup.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace MeuTrabalho
+namespace MeuTrabalhoNet5
 {
     public class Startup
     {
@@ -27,11 +26,10 @@ namespace MeuTrabalho
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -41,12 +39,18 @@ namespace MeuTrabalho
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Account}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Index}/{id?}");
             });
+
+            // app.UseAuthorization();
+
         }
     }
 }
